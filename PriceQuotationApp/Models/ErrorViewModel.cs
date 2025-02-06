08ca432558eb1d508ecd485d723f9a36d964f614
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-02-06 10:05:18 -0800

    Add project files.

---
+++ PriceQuotationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+// Ignore Spelling: App
+
+namespace PriceQuotationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
